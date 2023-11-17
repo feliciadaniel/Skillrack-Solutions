8688efe251e2f1015442a8abb03b333b3daac05d
feliciadaniel <100063610+feliciadaniel@users.noreply.github.com>
2023-11-17 13:18:45 +0530

    Update code.c

--- 1.Permutation-String Characters/code.c
+++ 1.Permutation-String Characters/code.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  char str[50];
+  int len;
+  scanf("%s",str);
+  len=strlen(str);
+  permute(str,0,len-1);
+  return 0;
+}
+void permute(char str[],int l,int r){
+  if (l==r)
+    printf("%s\n",str);
+  else{
+    for(int i= l;i<=r;i++){
+      swap(str,l,r);
+      permute(str,l+1,r);
+      swap(str,l,r); //backtracking
+    }
+  }
+}
+void swap(char str[],int x,int y){
+  char temp;
+  temp=str[x];
+  str[x]=str[y];
+  str[y]=temp;
+}
